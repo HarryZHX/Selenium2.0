6ce60e027c29a48c5c9d3972f2862603ab38c015
HarryZHX <harryhaoxuan@gmail.com>
2020-03-01 21:57:51 -0800

    First Commit

---
+++ src/test/java/HomePageTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+    public static void main(String[] arg){
+        // Go to the homepage https://tstkwidos.tk/
+        // Verify title is equal to kwidos
+        // turn the log off
+        System.setProperty("webdriver.chrome.silentOutput","true");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testkwidos.tk");
+        driver.manage().window().maximize();
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Kwidos");
+        driver.quit();
+    }
+}
